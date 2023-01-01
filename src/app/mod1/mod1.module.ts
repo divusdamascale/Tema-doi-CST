07c2235bb0666483116e93319548ae6b645cd90b
divusdamascale <dutudenis30@yahoo.com>
2023-01-01 16:54:33 +0200

    add 3 components to mod1

--- src/app/mod1/mod1.module.ts
+++ src/app/mod1/mod1.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Mod1RoutingModule } from './mod1-routing.module';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Comp1Component,
+    Comp2Component,
+    Comp3Component
+  ],
   imports: [
     CommonModule,
     Mod1RoutingModule
